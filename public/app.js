b9187ff9710f736440fa1886018084dff9b39e5a
tybenz12 <tbenz1@macalester.edu>
2021-02-13 11:36:00 -0700

    Starting the login functions

---
+++ public/app.js
@@ -0,0 +1,16 @@
+(function () {
+
+    const config = {
+        apiKey: "AIzaSyDM0YYvGQFSc9qy6jh2hpxZy_87B8eNc3o",
+        authDomain: "webapp-43db3.firebaseapp.com",
+        databaseURL: "https://webapp-43db3-default-rtdb.firebaseio.com",
+        storageBucket: "webapp-43db3.appspot.com",
+    };
+    firebase.initializeApp(config);
+
+    const txtEmail = document.getElementById("email");
+    const txtPassword = document.getElementById("password");
+    const btnLogin = document.getElementById("submitLogin");
+
+
+});
